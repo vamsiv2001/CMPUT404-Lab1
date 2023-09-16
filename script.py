bfbfb1169362ca59dfa71abf00a2e45bab5e74fb
Vamsi Vasamsetti <vasamset@cs.ualberta.ca>
2023-09-16 17:50:20 -0600

    Updated Script

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import requests
 print(requests.__version__)
 
 ghp = requests.get("http://google.com")
-print(ghp.text)
+#print(ghp.text)
 
 github_code = requests.get("https://raw.githubusercontent.com/vamsiv2001/cmput404-lab1/main/script.py")
 print(github_code.text)
